95bba4d9237e0c01c31a03f63daa57a70c6f15c0
ifeanyi anaga <ifeanyianaga95@gmail.com>
2023-06-15 20:39:48 +0100

    market

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -82,8 +82,8 @@ WSGI_APPLICATION = 'mywebsite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-"""
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
