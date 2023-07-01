f0f17cfd4290d500b3a28f84b65e5f7c903dec86
ifeanyi anaga <ifeanyianaga95@gmail.com>
2023-07-01 13:02:01 +0100

    change made in typed effect and database

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'mywebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -103,7 +103,7 @@ DATABASES = {
     }
 }
 
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
